4279ed0fd1a1d94980ebcf64d0ad2dd4082a7145
Zubair Khalid <msds20010@itu.edu.pk>
2021-11-12 23:17:16 +0500

    minting and burn code added

--- contracts/FaltuCoin.sol
+++ contracts/FaltuCoin.sol
@@ -5,10 +5,39 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FaltuCoin is ERC20 {
 
-    constructor(uint256 _supply) ERC20("FALTU", "FLTU") {
+    address public admin;
+    uint256 public mintLimit;
+
+    modifier onlyAuthorized() {
+        require(admin == msg.sender, "Not authorized to execute");
+        _;
+    }
+
+    modifier mintLimited(uint256 _amount){
+        require(_amount <= mintLimit, "Amound exceeded mint limit exceeded");
+        _;
+    }
+
+    constructor(uint256 _supply, uint256 _mintLimit) ERC20("FALTU", "FLTU") {
+
+        admin = msg.sender;
+        mintLimit = _mintLimit;
 
         _mint(msg.sender, _supply * (10 ** decimals()));
-        
+
+    }
+
+    function mint(address _to, uint256 _amount) public onlyAuthorized mintLimited(_amount){
+
+        require(_amount > 0, "Amount must be greater than zero");
+        require(_to != address(0), "Invalid recipient address");
+
+        _mint(_to, _amount * (10 ** decimals()));
+    }
+
+    function burn(uint256 _amount) public onlyAuthorized{
+        require(_amount > 0, "Amount must be greater than zero");
+        _burn(msg.sender, _amount);
     }
 }
 
